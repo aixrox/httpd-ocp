bd069f46c2223dccd01e8af320b5db84dedce73c
aixrox <kaloyankov@gmail.com>
2026-03-02 07:30:04 +0100

    Seed it from example

---
+++ tests/test_httpd_template.py
@@ -0,0 +1,35 @@
+import os
+
+import pytest
+from pathlib import Path
+
+from container_ci_suite.openshift import OpenShiftAPI
+
+test_dir = Path(os.path.abspath(os.path.dirname(__file__)))
+
+VERSION=os.getenv("SINGLE_VERSION")
+
+class TestHTTPDExTemplate:
+
+    def setup_method(self):
+        self.oc_api = OpenShiftAPI(pod_name_prefix="httpd-example")
+        json_raw_file = self.oc_api.get_raw_url_for_json(
+            container="httpd-container", dir="imagestreams", filename="httpd-rhel.json"
+        )
+        self.oc_api.import_is(path=json_raw_file, name="httpd")
+
+    def teardown_method(self):
+        self.oc_api.delete_project()
+
+    def test_httpd_ex_template_inside_cluster(self):
+        template_json = self.oc_api.get_raw_url_for_json(
+            container="httpd-ex", dir="openshift/templates", filename="httpd.json"
+        )
+        assert self.oc_api.deploy_template(
+            template=template_json, name_in_template="httpd-example", expected_output="Welcome to your static httpd",
+            openshift_args=["SOURCE_REPOSITORY_REF=master", f"HTTPD_VERSION={VERSION}", "NAME=httpd-example"]
+        )
+        assert self.oc_api.is_template_deployed(name_in_template="httpd-example")
+        assert self.oc_api.check_response_inside_cluster(
+            name_in_template="httpd-example", expected_output="Welcome to your static httpd"
+        )
